abb9a97eb09ebb8380d3345a1c457b5bcc55afb3
Navraj singh kaler <navrajkaler996@gmail.com>
2023-08-12 22:04:56 -0500

    Tabs functionality completed

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Tabs from '../components/Tabs';
 
 const Home: React.FC = () => {
 
+    //State to track which tab is active
+    const [activeTabIndex, setActiveTabIndex] = useState<Number>(2)
+
+    //Function to update setActiveIndex.
+    //This function is used in the Tabs component.
+    const clickHandler = (i: Number) => {
+        setActiveTabIndex(i)
+    }
+
     return <div className='home'>
+        {/* General information of the user */}
         <div className="home__info-container">        
             <p className='home__username text-1'>Eazy-eee</p>
             <p className='home__location'>Winnipeg, MB</p>
             <p className="home__status">Study Permit</p>
-            
         </div>
-
-        <Tabs />
+        {/* Tabs (three at a time)
+            activeTabIndex and clickHandler are passed as props.
+        */}
+        <Tabs activeTabIndex={activeTabIndex} clickHandler={clickHandler} />
     </div>
 
 }
