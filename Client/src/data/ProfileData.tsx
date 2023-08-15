11633fc97671212b12ebaf91396f5131ae0bba6f
Navraj singh kaler <navrajkaler996@gmail.com>
2023-08-15 10:53:26 -0500

    UI done for types - text and image

--- Client/src/data/ProfileData.tsx
+++ Client/src/data/ProfileData.tsx
@@ -5,7 +5,14 @@ const data = [
         text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type a scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap saelectronic typesetting, remaining essentially unchanged",
         group: "Winnipeg",
         postedAt: "2023-08-12T22:45:58-05:00"
-    }
+    },
+    {
+        type: "image",
+        title: "Who is your favourite character in Mirzapur?",
+         text: "",
+        group: "Mirzapur",
+        postedAt: "2023-08-12T22:45:58-05:00"
+    },
 ]
 
 export default data;
--- Client/src/pages/Profile.tsx
+++ Client/src/pages/Profile.tsx
@@ -1,30 +1,73 @@
+//image imports
 import Winnipeg from "../assets/Winnipeg.jpeg";
+import LikeButton from "../assets/like.svg";
+import DislikeButton from "../assets/dislike.svg";
+import CommentButton from "../assets/comment.svg";
+import ShareButton from "../assets/share.svg";
+import MunnaBhaiya from "../assets/munna-bhaiya.jpg";
+
+import ProfileData from "../data/ProfileData";
+
+interface postType {
+    type: string ,
+    text: string,
+    group: string,
+    postedAt: string,
+    title: string
+}
 
 const Profile: React.FC = () => {
 
+    //Function filter the type of post.
+    //This function returns the JSX of the filter post.
+    const getPostContent = (post: postType) => {
+
+        if(post.type === "text") {
+            return <p className="profile__post-content-description">    
+                    {post.text}
+                </p>
+        }
+        if(post.type === "image"){
+            return <img src={MunnaBhaiya} alt="post-image-demo" className="profile__post-content-image" />
+        }
+    }
+
     return <div className="profile">
-        <div className="profile__post">
-            <div className="profile__post-info">
-                <img src={Winnipeg} className="profile__post-info-dp"/>
-                <div className="profile__post-info-data">
-                    <span className="profile__post-info-data-group">Winnipeg</span>
-                    <span className="profile__post-info-data-postedAt">2 days ago</span>
-                </div>
-            </div>
-            <div className="profile__post-content">
-                <p className="profile__post-content-title">
-                    Anyone studying in the University of Winnipeg?
-                </p>
-                <p className="profile__post-content-description">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type a 
-                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap sa
-                    electronic typesetting, remaining essentially unchanged.
-                </p>
-            </div>
-            <div className="post-reactions">POST REACTIONS</div>
-        </div>
-        <div className="divider" style={{margin: "1rem 0"}}></div>
+
+        {ProfileData.map((post) => {
+            return <> <div className="profile__post">
+                        {/* Container for post info such as group name and dp */}
+                        <div className="profile__post-info">
+                            <img src={Winnipeg} className="profile__post-info-dp"/>
+                            <div className="profile__post-info-data">
+                                <span className="profile__post-info-data-group">{post.group}</span>
+                                <span className="profile__post-info-data-postedAt">1 days ago</span>
+                            </div>
+                        </div>
+                        {/* Container for post content */}
+                        <div className="profile__post-content">
+                            <p className="profile__post-content-title">
+                               {post.title}
+                            </p>
+                            {
+                                getPostContent(post)
+                            }
+                            
+                        </div>
+                        {/* Container for post reaction */}
+                        <div className="profile__post-reactions">
+                            <div className="profile__post-reactions-1">
+                                <img src={LikeButton} alt="like-button" className="profile__post-reactions-1-like" />
+                                <img src={DislikeButton} alt="dislike-button" className="profile__post-reactions-1-dislike" />
+                                <img src={CommentButton} alt="comment-button" className="profile__post-reactions-1-comments" />
+                            </div>
+                            <div className="profile__post-reactions-2">
+                                <img src={ShareButton} alt="share-button" className="profile__post-reactions-2-share" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="divider" style={{margin: "1rem auto"}}></div></>
+        })}   
     </div>
 }
 
