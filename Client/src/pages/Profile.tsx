bd3e0431f35d9793f8a56b7e02abce517c48af04
Navraj singh kaler <navrajkaler996@gmail.com>
2023-08-15 09:10:40 -0500

    Progress on post container... (15/08/2023)

--- Client/src/pages/Profile.tsx
+++ Client/src/pages/Profile.tsx
@@ -11,7 +11,17 @@ const Profile: React.FC = () => {
                     <span className="profile__post-info-data-postedAt">2 days ago</span>
                 </div>
             </div>
-            <div className="profile__post-content">POST CONTENT</div>
+            <div className="profile__post-content">
+                <p className="profile__post-content-title">
+                    Anyone studying in the University of Winnipeg?
+                </p>
+                <p className="profile__post-content-description">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type a 
+                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap sa
+                    electronic typesetting, remaining essentially unchanged.
+                </p>
+            </div>
             <div className="post-reactions">POST REACTIONS</div>
         </div>
         <div className="divider" style={{margin: "1rem 0"}}></div>
