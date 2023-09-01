e5f8e9cc163a492b79528bbe388d3d5e79619f71
Navraj singh kaler <navrajkaler996@gmail.com>
2023-09-01 13:45:33 -0500

    Changes made in the UI

--- Client/src/components/CreatePost.tsx
+++ Client/src/components/CreatePost.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Post } from "../model/post.model";
 
 const createPostStyles = {
     type: {
@@ -6,19 +7,29 @@ const createPostStyles = {
     }
 }
 
+
+
+
 const CreatePost: React.FC = () => {
 
-    const [isFocused, setIsFocused] = useState(false)
+    const [isFocused, setIsFocused] = useState(false);
+
+    const [newPost, setPost] = useState<Post>()
+
+    const onFucus = () => {
+        return isFocused ? createPostStyles.type : {display: "none"}
+    }
 
     return <div className="create-post">
-        <div className="create-post__type" style={isFocused ? createPostStyles.type : {display: "none"}}>
-            <div className="create-post__type-text">Text</div>
+        <div className="create-post__type" style={onFucus()}>
+            <div className="create-post__type-text" id="text">Text</div>
             <div className="create-post__type-image">Image</div>
             <div className="create-post__type-poll">Poll</div>
             <div className="create-post__type-link">Link</div>
         </div>
+        <input className="create-post__input" type="text" name="text" id="text" placeholder="Create a title..." style={onFucus()}/>
         <textarea className="create-post__textarea" placeholder="Post something..." onFocus={()=>setIsFocused(true)} />
-        <div className="create-post__buttons" style={isFocused ? createPostStyles.type : {display: "none"}}>
+        <div className="create-post__buttons" style={onFucus()}>
             <button className="create-post__post btn">Submit</button>
         </div>
     </div>
